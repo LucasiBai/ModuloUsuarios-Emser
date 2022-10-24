71b70a76cfb3e5d1fd10937271e9db897cea6ec5
LucasiBai <lucasibaigorria@mi.unc.edu.ar>
2022-10-24 19:59:28 -0300

    agregando admin personalizado de user + respectivos tests

--- db/admin.py
+++ db/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext
 
-# Register your models here.
+from db import models
+
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ["id"]
+    list_display = ["email", "first_name", "last_name"]
+
+    fieldsets = (
+        (None, {"fields": ["email", "password"]}),
+        (gettext("Personal Info"), {"fields": ["first_name", "last_name"]}),
+        (gettext("Permissions"), {"fields": ["is_active", "is_staff", "is_superuser"]}),
+        (gettext("Important Dates"), {"fields": ["last_login"]}),
+    )
+
+
+admin.site.register(models.UserAccount, UserAdmin)
--- db/tests/test_admin.py
+++ db/tests/test_admin.py
@@ -4,4 +4,42 @@ from django.urls import reverse
 
 
 class ModelTest(TestCase):
-    pass
+    def setUp(self):
+        """
+        Set up method
+        """
+        self.client = Client()
+
+        # Creating super user
+        self.super_user = get_user_model().objects.create_superuser(
+            email="testadmin@mitest.com", password="pass1234"
+        )
+        self.client.force_login(self.super_user)
+
+        # Creating Normal User
+        self.user = get_user_model().objects.create_user(
+            email="testuser@mitest.com", password="pass1234", first_name="TestUserName"
+        )
+
+    def test_user_listed(self):
+        """
+        Tests if the users added were listed
+        """
+        url = reverse("admin:db_useraccount_changelist")
+        res = self.client.get(url)
+
+        # Testing if normal user is listed
+        self.assertContains(res, self.user.first_name)
+        self.assertContains(res, self.user.email)
+
+        # Testing if super user is listed
+        self.assertContains(res, self.super_user.email)
+
+    def test_user_change_page(self):
+        """
+        Tests if the change page work
+        """
+        url = reverse("admin:db_useraccount_change", args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
